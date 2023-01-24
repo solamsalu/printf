ef097dfc0da1ce88a204b9440bb0a01e03f59d3e
tekalign <tekalign1991@gmail.com>
2023-01-24 14:07:55 -0800

    printf file

--- _printf.c
+++ _printf.c
@@ -1,51 +1,59 @@
 #include "main.h"
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Produces output according to a format
- * @format: Is a character string. The format string
- * is composed of zero or more directives
- *
- * Return: The number of characters printed (excluding
- * the null byte used to end output to strings)
- **/
+ * @format: Is a character string
+ * Return: The number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0;
-	int (*f)(va_list);
-	va_list args;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(args, format);
-	if (format == NULL || !format[i + 1])
+	if (format == NULL)
 		return (-1);
-	while (format[i])
+	va_start(list, format);
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (format[i + 1])
-			{
-				if (format[i + 1] != 'c' && format[i + 1] != 's'
-				    && format[i + 1] != '%' && format[i + 1] != 'd'
-				    && format[i + 1] != 'i')
-				{
-					j += _putchar(format[i]);
-					j += _putchar(format[i + 1]);
-					i++;
-				}
-				else
-				{
-					f = get_func(&format[i + 1]);
-					j += f(args);
-					i++;
-				}
-			}
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
 		}
 		else
 		{
-			_putchar(format[i]);
-			j++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		i++;
 	}
-	va_end(args);
-	return (j);
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer
+ * @buffer: Array of characters
+ * @buff_ind: Index to add next char, represent length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
