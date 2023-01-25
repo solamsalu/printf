f33836a5c5546137c268cb656394bfe46157d19f
tekalign <tekalign1991@gmail.com>
2023-01-24 17:19:42 -0800

    functions

--- _printf.c
+++ _printf.c
@@ -1,59 +1,91 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * _printf - Produces output according to a format
- * @format: Is a character string
- * Return: The number of characters printed
+ * printIdentifiers - prints special characters
+ *
+ *  @next: character after the %
+ *
+ *  @arg: argument for the indentifier
+ *
+ *  Return: the number of characters printed
+ *
  */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++)
+int printIdentifiers(char next, va_list arg)
+
+{
+	int functsIndex;
+
+	identifierStruct functs[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int},
+		{"u", print_unsigned},
+		{"b", print_unsignedToBinary},
+		{"o", print_oct},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"S", print_STR},
+		{NULL, NULL}
+	};
+	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
 	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+		if (functs[functsIndex].indentifier[0] == next)
+			return (functs[functsIndex].printer(arg));
 	}
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (printed_chars);
+	return (0);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer
- * @buffer: Array of characters
- * @buff_ind: Index to add next char, represent length.
+ * _printf - mimic printf from stdio
+ * Description: produces output according to a format stream
+ * @format: character string composed of zero or more directives
+ * Return: the number of characters printed
+ * return -1 for incomplete identifier error
  */
-void print_buffer(char buffer[], int *buff_ind)
+
+int _printf(const char *format, ...)
 
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
+	unsigned int i;
+	int identifierPrinted = 0, charPrinted = 0;
+	va_list arg;
+
+	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			charPrinted++;
+			continue;
+		}
+		if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			charPrinted++;
+			i++;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+			return (-1);
+		identifierPrinted = printIdentifiers(format[i + 1], arg);
+		if (identifierPrinted == -1 || identifierPrinted != 0)
+			i++;
+		if (identifierPrinted > 0)
+			charPrinted += identifierPrinted;
+		if (identifierPrinted == 0)
+		{
+			_putchar('%');
+			charPrinted++;
+		}
+	}
+	va_end(arg);
+	return (charPrinted);
 }
