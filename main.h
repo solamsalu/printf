99389b38b8e1c8ded6915e2036fd4af7a3399c34
tekalign <tekalign1991@gmail.com>
2023-01-24 16:06:19 -0800

    tasks

--- main.h
+++ main.h
@@ -6,7 +6,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-<<<<<<< HEAD
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
@@ -124,34 +123,6 @@ struct fmt
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
-=======
-int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
-int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int* printed);
-int print_unsignedToBinary(va_list arg);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR (va_list arg);
-
-/**
- * struct identifierStruct - structure definition of a printTypeStruct
- * @indentifier: type
- * @printer: function to print
- */
-typedef struct identifierStruct
-{
-char *indentifier;
-int (*printer)(va_list);
-} identifierStruct;
->>>>>>> 485877da3468c23e81acc6c0f75221501b881153
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
